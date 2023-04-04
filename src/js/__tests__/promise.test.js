395ddef0bf2285fb439dfb3bccb03065bda42d35
Анастасия Ельшина <salnaya92@gmail.com>
2023-04-04 17:48:40 +0300

    promise test was added

---
+++ src/js/__tests__/promise.test.js
@@ -0,0 +1,18 @@
+import GameSaving from '../GameSaving';
+import GameSavingLoader from '../GameSavingLoader';
+
+test('load json from GameSaving', async (done) => {
+  const gameSaving = new GameSaving({
+    id: 9,
+    created: 1546300800,
+    userInfo: {
+      id: 1,
+      name: 'Hitman',
+      level: 10,
+      points: 2000,
+    },
+  });
+  const result = await GameSavingLoader.load();
+  expect(result).toEqual(gameSaving);
+  done();
+});
